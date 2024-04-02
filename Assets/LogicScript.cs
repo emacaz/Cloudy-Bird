0f91eed186ec949dc2271da8b5eb2382be6ff597
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-04-02 16:06:42 +0200

    Finish collision logic

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
 	public int playerScore;
 	public Text scoreText;
+  public GameObject gameOverScreen;
 
 	[ContextMenu("Increase Score")]
 	public void addScore(int scoreToAdd)
@@ -14,4 +16,14 @@ public class LogicScript : MonoBehaviour
 		playerScore = playerScore + scoreToAdd;
 		scoreText.text = playerScore.ToString();
 	}
+
+  public void restartGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+
+  public void gameOver()
+  {
+    gameOverScreen.SetActive(true);
+  }
 }
